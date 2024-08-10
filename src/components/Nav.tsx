4d1fac3f610c99543f9ac9f876ddd931641630f5
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2024-08-11 02:35:14 +0530

    hide signout button if no user

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,10 +1,12 @@
 import { useColorMode } from '@kobalte/core';
-import { action, redirect } from '@solidjs/router';
+import { action, redirect, useLocation } from '@solidjs/router';
 import { eq } from 'drizzle-orm';
+import { Show, createResource } from 'solid-js';
 import { getRequestEvent } from 'solid-js/web';
 import { deleteCookie, getCookie } from 'vinxi/http';
 import { db } from '~/db';
 import { refreshTokens } from '~/db/schema';
+import { getUser } from '~/db/utils/users';
 import { cn } from '~/lib/utils';
 import { Button } from './ui/button';
 
@@ -20,6 +22,12 @@ const signOut = action(async () => {
 }, 'signout');
 
 export default function Nav(props: { class?: string }) {
+	const location = useLocation();
+	const [user] = createResource(
+		() => location.pathname,
+		() => getUser()
+	);
+
 	const { toggleColorMode } = useColorMode();
 
 	return (
@@ -29,14 +37,14 @@ export default function Nav(props: { class?: string }) {
 					<p class="font-bold uppercase tracking-wide">Kanban</p>
 				</a>
 				<span class="grow" />
-
-				<form action={signOut} method="post">
-					<Button type="submit" class="flex items-center gap-2" variant="outline">
-						<span>Sign Out</span>
-						<span class="i-heroicons:arrow-right-end-on-rectangle text-xl"></span>
-					</Button>
-				</form>
-
+				<Show when={user()}>
+					<form action={signOut} method="post">
+						<Button type="submit" class="flex items-center gap-2" variant="outline">
+							<span>Sign Out</span>
+							<span class="i-heroicons:arrow-right-end-on-rectangle text-xl"></span>
+						</Button>
+					</form>
+				</Show>
 				<Button onClick={() => toggleColorMode()} variant="outline" size="icon">
 					<div class="i-heroicons:sun rotate-0 scale-100 text-xl transition-all dark:-rotate-90 dark:scale-0" />
 					<div class="i-heroicons:moon absolute rotate-90 scale-0 text-xl transition-all dark:rotate-0 dark:scale-100" />

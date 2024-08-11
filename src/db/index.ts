dd624289492f7b1be404c155fc0e4c3bc77a8c69
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2024-08-11 18:56:25 +0530

    make auth token optional

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,7 +3,7 @@ import { drizzle } from 'drizzle-orm/libsql';
 
 const client = createClient({
 	url: process.env.TURSO_CONNECTION_URL!,
-	authToken: process.env.TURSO_AUTH_TOKEN!
+	authToken: process.env.TURSO_AUTH_TOKEN
 });
 
 export const db = drizzle(client);

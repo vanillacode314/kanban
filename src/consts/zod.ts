35a3335f62f612be214948bb2f5b8a7c5bbdfadd
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2024-08-28 22:29:59 +0530

    fix password regex

--- src/consts/zod.ts
+++ src/consts/zod.ts
@@ -6,9 +6,9 @@ const passwordSchema = z
 	.regex(/(?=.*[0-9])/, 'Password must contain a number')
 	.regex(/(?=.*[a-z])/, 'Password must contain a lowercase letter')
 	.regex(/(?=.*[A-Z])/, 'Password must contain an uppercase letter')
-	.regex(/(?=.*[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~])/, 'Password must contain a special character')
+	.regex(/(?=.*[!"#$%&'()*+,-./:;<=>?@[\\\]^_`{|}~])/, 'Password must contain a special character')
 	.regex(
-		/(?<![^A-Za-z0-9!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~].*)[A-Za-z0-9!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]+(?!.*[^A-Za-z0-9!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~])/,
-		'Password can only contain letters, numbers, and special characters !@#$%^&*'
+		/(?<![^A-Za-z0-9!"#$%&'()*+,-./:;<=>?@[\\\]^_`{|}~].*)[A-Za-z0-9!"#$%&'()*+,-./:;<=>?@[\\\]^_`{|}~]+(?!.*[^A-Za-z0-9!"#$%&'()*+,-./:;<=>?@[\\\]^_`{|}~])/,
+		'Password can only contain letters, numbers, and special characters !"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
 	);
 export { passwordSchema };

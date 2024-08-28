dae592671dc5c3230cf16b37495a8a6c6cd1f2f7
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2024-08-29 03:44:06 +0530

    update maskable image

--- pwa-assets.config.ts
+++ pwa-assets.config.ts
@@ -1,6 +1,11 @@
-import { defineConfig } from '@vite-pwa/assets-generator/config';
+import { Preset, defineConfig } from '@vite-pwa/assets-generator/config';
 
+const preset = {
+	transparent: { sizes: [64, 192, 512], favicons: [[48, 'favicon.ico']] },
+	maskable: { sizes: [512] },
+	apple: { sizes: [180] }
+} satisfies Preset;
 export default defineConfig({
-	preset: 'minimal-2023',
+	preset,
 	images: ['public/favicon.svg']
 });
